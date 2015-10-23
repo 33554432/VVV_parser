6d21762b393dc8cefd2774cf0ca0b05384507fca
Daniel M. Weeks <dan@danweeks.net>
2015-10-23 12:46:45 -0400

    Operators should be surrounded by spaces

--- VVV parser.py
+++ VVV parser.py
@@ -13,7 +13,7 @@ def fileparser ():
             print ('...')
         else:
             shutil.move (os.path.join (d, file), os.path.join(dst1, file))
-    if len (os.listdir(os.path.join(d, r'Thumb')))== 0:
+    if len (os.listdir(os.path.join(d, r'Thumb'))) == 0:
             os.rmdir(os.path.join(d, r'Thumb'))
     ftype = ['RFP', 'DIC', 'GFP']
     x = 0
@@ -25,15 +25,15 @@ def fileparser ():
         maxsp = int (input('What is the max number of stage positions per plate used? '))
         while count <= maxsp:
             alist.append (count)
-            count +=1
+            count += 1
         b = len (alist)
         aval = 1
-        while aval <=8:
+        while aval <= 8:
             while x < len (ftype):
                 while 0 < b <= len(alist):
                     os.mkdir (os.path.join (d, str(ftype [x]) + '_SAMPLE' + str(aval) + '_S' + str (alist [b-1])))
                     dst2 = os.path.join (d, str(ftype [x]) + '_SAMPLE' + str(aval) + '_S' + str (alist [b-1]))
-                    search = re.compile ('Sample' + str(aval) + '.*' + str (ftype [x])+ '.*' + '_s' + str (alist [b-1]))
+                    search = re.compile ('Sample' + str(aval) + '.*' + str (ftype [x]) + '.*' + '_s' + str (alist [b-1]))
                     for file in os.listdir(d):
                         if re.search( search, file) is None:
                             print (search, aval)
@@ -41,23 +41,23 @@ def fileparser ():
                             shutil.move (os.path.join(d, file), os.path.join(dst2, file))
                     if len (os.listdir(dst2)) == 0:
                         os.rmdir(dst2)
-                    b -=1
-                x +=1
+                    b -= 1
+                x += 1
                 b = len (alist)
-            aval +=1
+            aval += 1
             x = 0
             b = len(alist)
     else:
         stagepos = int (input ('How many stage positions? '))
         while count <= stagepos:
             alist.append (count)
-            count +=1
+            count += 1
         b = len (alist)
         while x < len (ftype):
-            while 0< b <= len (alist):
+            while 0 < b <= len (alist):
                 os.mkdir (os.path.join (d, str(ftype [x]) + '_S' + str (alist [b-1])))
                 dst2 = os.path.join (d, str(ftype [x]) + '_S' + str (alist [b-1]))
-                search = re.compile (str (ftype [x])+ '.*' +'_s' + str (alist [b-1]))
+                search = re.compile (str (ftype [x]) + '.*' + '_s' + str (alist [b-1]))
                 for file in os.listdir(d):
                     if re.search( search, file) is None:
                         print (b, x)
@@ -65,6 +65,6 @@ def fileparser ():
                         shutil.move (os.path.join(d, file), os.path.join(dst2, file))
                 if len (os.listdir(dst2)) == 0:
                     os.rmdir(dst2)
-                b -=1
-            x +=1
+                b -= 1
+            x += 1
             b = len(alist)
